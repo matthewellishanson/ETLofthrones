19f28b34b7953fbd84cba90e3af8719412359370
Megan Kirby <megankirby@stp-er-10-129-194-222.wireless.umn.edu>
2019-05-15 19:27:13 -0500

    adding sql file

---
+++ ETLofThrones.sql
@@ -0,0 +1,46 @@
+create database ETLofThrones;
+use ETLofThrones;
+
+create table Characters (
+Character_ID integer primary key auto_increment,
+FirstName varchar(50),
+LastName varchar(100),
+Gender boolean,
+Book1 boolean,
+Book2 boolean,
+Book3 boolean,
+Book4 boolean,
+Book5 boolean,
+House varchar(50),
+DeathStatus boolean,
+Death_ID integer
+);
+
+create table Deaths (
+Death_ID integer primary key auto_increment,
+Character_ID integer,
+FirstName varchar(50),
+LastName varchar(50),
+Book_of_Death integer,
+DeathYear integer
+);
+
+create table Battles (
+Battle_ID integer primary key auto_increment,
+BattleName varchar(100),
+Battle_Year integer,
+Attacker varchar(150),
+Defender varchar(150),
+Attacker_Outcome varchar(10)
+);
+
+create table Discussion (
+Discussion_ID integer primary key auto_increment,
+Character_ID integer,
+FirstName varchar(50),
+LastName varchar(50),
+Popularity float
+);
+
+
+
